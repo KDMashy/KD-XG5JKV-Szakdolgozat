c4a8af4eef3e926c1de8d0cacf5bb960a8b56aa4
Dominik-Klepe <dominik.klepe@cubicfox.com>
2022-11-19 03:01:19 +0100

    validations added

---
+++ frontend/src/validations/index.tsx
@@ -0,0 +1,40 @@
+import * as yup from "yup";
+
+const registrationValidation = yup.object().shape({
+  username: yup.string().min(5).max(16).required("Username is required"),
+  first_name: yup.string().required("First name is required"),
+  last_name: yup.string().required("Last name is required"),
+  email: yup
+    .string()
+    .email("Please enter a valid email")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .min(10, ({ min }) => `Enter at least ${min} characters`)
+    .required("Password is required")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/,
+      "Password must contain at least one uppercase and one lowercase character"
+    ),
+  confirmation: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords do not match")
+    .required("Confirm password is required"),
+});
+
+const loginValidation = yup.object().shape({
+  username: yup
+    .string()
+    .email("Please enter a valid email")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .min(10, ({ min }) => `Enter at least ${min} characters`)
+    .required("Password is required")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/,
+      "Password must contain at least one uppercase and one lowercase character"
+    ),
+});
+
+export { registrationValidation, loginValidation };
